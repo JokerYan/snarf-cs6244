950b620bc1312d4ead4f3225bc9457876f67532f
JokerYan <770792058@qq.com>
2023-03-18 23:41:05 +0800

    tile max_idx before gather

--- lib/model/deformer.py
+++ lib/model/deformer.py
@@ -178,7 +178,9 @@ class ForwardDeformer(torch.nn.Module):
         with torch.enable_grad():
             xc, _ = self.forward(xd, cond, tfs, eval_mode=eval_mode)        # B x N x I x D, I is number of init
             print("xc shape", xc.shape)
-            xc = torch.gather(xc, dim=2, index=max_idx[..., None])          # B x N x 1 x D
+            D = xc.shape[-1]
+            max_idx = torch.tile(max_idx[..., None], [1, 1, 1, D])          # B x N x 1 x D
+            xc = torch.gather(xc, dim=2, index=max_idx)                     # B x N x 1 x D
             print("xc shape", xc.shape)
         xd_last = self.forward_skinning(xc, cond, tfs_last)
         exit()
