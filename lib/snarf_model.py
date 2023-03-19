1c3e1ed344920a795ba8954e58c4bb20174ccf71
JokerYan <770792058@qq.com>
2023-03-19 16:57:14 +0800

    remove fast mode overwrite

--- lib/snarf_model.py
+++ lib/snarf_model.py
@@ -174,8 +174,6 @@ class SNARFModel(pl.LightningModule):
         return self.validation_epoch_end(test_step_outputs)
 
     def plot(self, data, res=128, verbose=True, fast_mode=False):
-        fast_mode = False   # overwrite
-
         res_up = np.log2(res//32)
 
         if verbose:
